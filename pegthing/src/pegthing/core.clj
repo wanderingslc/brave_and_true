97f5f5f11f753061adf38f1a4a27276e0db3402c
wanderingslc <stan.martineau@gmail.com>
2020-05-27 07:58:12 -0600

    Work through the end of the chapter

--- pegthing/src/pegthing/core.clj
+++ pegthing/src/pegthing/core.clj
@@ -178,5 +178,45 @@
   (let [input (map letter->pos (characters-as-strings (get-input)))]
     (if-let [new-board (make-move board (first input) (second input))]
       (user-entered-valid-move new-board)
-      (user-entered-valid-move board))))
+      (user-entered-invalid-move board))))
 
+(defn user-entered-invalid-move
+  "Handles the next step after a user has entered an invalid move"
+  [board]
+  (println "\n!!! That was an invalid move :( \n")
+  (prompt-move board))
+
+(defn user-entered-valid-move
+  "Handles next step after good move passed in"
+  [board]
+  (if (can-move? board)
+    (prompt-move board)
+    (game-over board)))
+
+(defn game-over
+  "Announce the game is over and prompt to play again"
+  [board]
+  (let [remaining-pegs (count (filter :pegged (vals board)))]
+    (println "Game Over! You had " remaining-pegs " pegs left")
+    (print-board board)
+    (println "Play again? y/n [y]")
+    (let [input (get-input "y")]
+      (if (= "y" input )
+        (prompt-rows)
+        (do
+          (println "Bye!")
+          (System/exit 0))))))
+
+(defn prompt-empty-peg
+  [board]
+  (println "Here's your board: ")
+  (print-board board)
+  (println "Remove which peg? [e]")
+  (prompt-move (remove-peg board (letter->pos (get-input "e")))))
+
+(defn prompt-rows
+  []
+  (println "How many rows? [5]")
+  (let [rows (Integer. (get-input 5))
+        board (new-board rows)]
+    (prompt-empty-peg board)))
