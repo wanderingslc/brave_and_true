cf3161a4f36945b81c6338b0532447917a1c7ebc
wanderingslc <stan.martineau@gmail.com>
2020-05-26 21:52:55 -0600

    Adds the pegthing project

---
+++ pegthing/src/pegthing/core.clj
@@ -0,0 +1,59 @@
+(ns pegthing.core
+  (require [clojure.set :as set])
+  (:gen-class))
+
+(declare successful-move prompt-move game-over query-rows)
+
+(defn tri*
+  "Generates lazy sequence of triangular numbers"
+  ([] (tri* 0 1))
+  ([sum n]
+   (let [new-sum (+ sum n)]
+     (cons new-sum (lazy-seq (tri* new-sum (inc n)))))))
+
+(defn triangular?
+  "Check if the number is triangular"
+  [n]
+  (= n (last (take-while #(>= n %) tri))))
+
+(defn row-tri
+  "The triangular number at the end of row n"
+  [n]
+  (last (take n tri)))
+
+(defn row-num
+  [pos]
+  (inc (count (take-while #(> pos %) tri))))
+
+(defn connect
+  "Form a connection between two positions"
+  [board max-pos pos neighbor destination]
+  (if (<= destination max-pos)
+    (reduce (fn [new-board [p1 p2]]
+              (assoc-in new-board [p1 :connections p2] neighbor))
+            board
+            [[pos destination] [destination pos]])
+    board))
+
+(defn connect-right
+  [board max-pos pos]
+  (let [neighbor (inc pos)
+        destination (inc neighbor)]
+    (if-not (or (triangular? neighbor) (triangular? pos))
+      (connect board max-pos pos neighbor destination)
+      board)))
+
+(defn connect-down-left
+  [board max-pos pos]
+  (let [row (row-num pos)
+        neighbor (+ row pos)
+        destination (+ 1 row neighbor)]
+    (connect board max-pos pos neighbor destination)))
+
+(defn connect-down-right
+  [board max-pos pos]
+  (let [row (row-num pos)
+        neighbor (+ 1 row pos)
+        destination (+ 2 row neighbor)]
+    (connect board max-pos pos neighbor destination)))
+
